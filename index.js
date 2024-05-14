7f2a59baff3bd0cd715f7c6ee138e247a5c3fd57
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-05-14 17:11:00 +0000

    Read all [GET] /item implementado

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 // Desafio: criar uma rota [GET] /oi que retorna "Olá, mundo"
 
 app.get('/oi', function (req, res) {
-    res.send('Olá, mundo');
-})
+    res.send('Olá, mundo!');
+});
 
-app.listen(3000)
+// Lista de itens
+
+const itens = 
+    ["Rick sanches", 
+    "Morty Smith", 
+    "Summer Smith"];
+
+// Endpoint de Read All [GET] /item
+
+app.get('/item', function (req, res) {
+  res.send(itens);
+});
+
+app.listen(3000);
